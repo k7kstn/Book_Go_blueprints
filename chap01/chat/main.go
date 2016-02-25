6b857a35b4a5f6b006971f81605b2364ae62d1b6
K7kstn ICT <ict@kessetsu.net>
2016-02-25 19:19:02 +0900

    O'Reilly Book: Go Programming Blueprints  lesson codes - Chap1 chat app
    initial -> 2nd modification:  template structure
    
      separate HTML part as template.

--- chap01/chat/main.go
+++ chap01/chat/main.go
@@ -3,21 +3,31 @@ package main
 import (
   "log"
   "net/http"
+  "text/template"
+  "path/filepath"
+  "sync"
 )
 
-func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte(`
-      <html>
-        <head>
-	  <title>Chat チャット</title>
-	</head>
-        <body>
-	  チャットしましょう！ / Let's chat !!
-	</body>
-      </html>
-    `))
+// templ は１つのテンプレートを表します templ indicates 1 template
+type templateHandler struct {
+  once     sync.Once
+  filename string
+  templ    *template.Template
+}
+
+// ServeHTTP は HTTPリクエストを処理します handles HTTP requests
+func  (t *templateHandler)  ServeHTTP(w http.ResponseWriter, r *http.Request) { 
+  t.once.Do(func()  {
+    t.templ = 
+        template.Must(template.ParseFiles(filepath.Join("templates",
+	    t.filename)))
   })
+  t.templ.Execute(w, nil)  // return value should be checked.
+}
+
+func main() {
+  // Root
+  http.Handle("/", &templateHandler{filename: "chat.html"})
 
   // Web サーバーを開始します / Start HTTP Server
   if err  := http.ListenAndServe(":8080", nil);  err != nil {
