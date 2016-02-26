641274ffa33a4e0ddfe92bc87b6e0731f4f953ff
K7kstn ICT <ict@kessetsu.net>
2016-02-26 18:13:49 +0900

    O'Reilly Book: Go Programmin Blueprints  lesson codes - Chap1 chat app
    Char Room modeling :  struct mod   add join, leave channel and  clients map

--- chap01/chat/room.go
+++ chap01/chat/room.go
@@ -4,5 +4,17 @@ type room struct {
   // forward は他のクライアントに転送するためのメッセージを保持するチャネルです。
   // forward is a channel to hold message which is to be forwarded to other clients.
   forward chan []byte
+
+  // join はチャットルームに参加しようとしているクライアントのためのチャネルです。
+  // join  is a channel for clients aim to join the Chat Room.
+  join chan *client
+
+  // leave はチャットルームから退室しようとしているクライアントのためのチャネルです。
+  // leave  is a channel for clients aim to leave the Chat Room.
+  leave chan *client
+
+  // clients には在室しているすべてのクライアントが保持されます。
+  // clients  keep all clients in the Chat Room.
+  clients map[*client]bool
 }
 
