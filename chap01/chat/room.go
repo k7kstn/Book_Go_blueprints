5513d19333a9bba3ab3a1da89f5f888b840ca93e
K7kstn ICT <ict@kessetsu.net>
2016-03-03 00:34:02 +0900

    O'Reilly Book: Go Programming Blueprints  lesson codes - Chap1 chat app
    Chat Room   HTTP Handler ( mod on  room.go )

--- chap01/chat/room.go
+++ chap01/chat/room.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+  "log"
+  "net/http"
+  "github.com/gorilla/websocket"
+)
+
 type room struct {
   // forward は他のクライアントに転送するためのメッセージを保持するチャネルです。
   // forward is a channel to hold message which is to be forwarded to other clients.
@@ -42,5 +48,31 @@ func (r *room) run() {
       }
     }
   }
+}
+
+const (
+  socketBufferSize = 1024
+  messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize:
+    socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+  socket, err := upgrader.Upgrade(w, req, nil)
+  if err != nil {
+    log.Fatal("ServeHTTP:", err)
+    return
+  }
+  client := &client{
+    socket: socket, 
+    send: make(chan []byte, messageBufferSize),
+    room: r,
+  }
+
+  r.join <- client
+  defer func() { r.leave <- client }()
+  go client.write()
+  client.read()
 }
 
