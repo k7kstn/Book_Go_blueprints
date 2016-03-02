178a1fe77e26ec6e88a8c050638a89da66b6b5bd
K7kstn ICT <ict@kessetsu.net>
2016-03-03 01:01:10 +0900

    O'Reilly Book: Go Programming Blueprints  lesson codes - Chap1 chat app
    Chat Room gen and use process added  in main.go

--- chap01/chat/main.go
+++ chap01/chat/main.go
@@ -27,7 +27,12 @@ func  (t *templateHandler)  ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
   // Root
+  r := newRoom()
   http.Handle("/", &templateHandler{filename: "chat.html"})
+  http.Handle("/room", r)
+
+  // チャットルームを開始します / Start Chat Room
+  go r.run()
 
   // Web サーバーを開始します / Start HTTP Server
   if err  := http.ListenAndServe(":8080", nil);  err != nil {
